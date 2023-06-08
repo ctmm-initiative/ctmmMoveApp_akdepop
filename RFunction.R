9c8753d3d9caf168acf3ba3864713d5803c01409
Marco Sciaini <marco@kaldera.dev>
2023-06-08 13:55:45 +0200

    update

--- RFunction.R
+++ RFunction.R
@@ -3,7 +3,7 @@ library(ctmm)
 rFunction = function(data) {
   
   pdf(appArtifactPath("akdepop.pdf"))
-  res <- meta(data)
+  res <- meta(data[[2]])
   dev.off()
   
   capture.output(res, file = appArtifactPath("akdepop_summary.txt"))
